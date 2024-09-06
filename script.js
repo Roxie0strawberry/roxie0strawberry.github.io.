8c61989503a501f38d045f9e19fce957f7f2c1a6
Roxie Strawberry (Marketing Gal) <love0thayna@gmail.com>
2024-09-06 15:19:02 +0100

    WORKING ON ABOUT PAGE STILL NEED TO FIX NAVS

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-          nav = document.getElementById(navId)
-  
-    toggle.addEventListener('click', () =>{
-        // Add show-menu class to nav menu
-        nav.classList.toggle('show-menu')
-  
-        // Add show-icon to show and hide the menu icon
-        toggle.classList.toggle('show-icon')
-    })
-  }
-  
-  showMenu('nav-toggle','nav-menu')
-  
- 
- 
 
   //FOR CONTACT ME PAGE//
 
 //FOR ABOUT ME PAGE//
 
+var pieChartValues = [{
+  y: 39.16,
+  exploded: true,
+  indexLabel: "Hello",
+  color: "#1f77b4"
+}, {
+  y: 21.8,
+  indexLabel: "Hi",
+  color: "#ff7f0e"
+}, {
+  y: 21.45,
+  indexLabel: "pk",
+  color: " #ffbb78"
+}, {
+  y: 5.56,
+  indexLabel: "one",
+  color: "#d62728"
+}, {
+  y: 5.38,
+  indexLabel: "two",
+  color: "#98df8a"
+}, {
+  y: 3.73,
+  indexLabel: "three",
+  color: "#bcbd22"
+}, {
+  y: 2.92,
+  indexLabel: "four",
+  color: "#f7b6d2"
+}];
+renderPieChart(pieChartValues);
 
+function renderPieChart(values) {
+
+  var chart = new CanvasJS.Chart("pieChart", {
+    backgroundColor: "white",
+    colorSet: "colorSet2",
+
+    title: {
+      text: "Pie Chart",
+      fontFamily: "Verdana",
+      fontSize: 25,
+      fontWeight: "normal",
+    },
+    animationEnabled: true,
+    data: [{
+      indexLabelFontSize: 15,
+      indexLabelFontFamily: "Monospace",
+      indexLabelFontColor: "darkgrey",
+      indexLabelLineColor: "darkgrey",
+      indexLabelPlacement: "outside",
+      type: "pie",
+      showInLegend: false,
+      toolTipContent: "<strong>#percent%</strong>",
+      dataPoints: values
+    }]
+  });
+  chart.render();
+}
